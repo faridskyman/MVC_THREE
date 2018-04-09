38524e49419a9b632b847ac1dee4f3065a20413b
Farid Mohd Ismail <farid@wizlearn.com>
2018-04-09 14:43:25 +0800

    Add project files.

---
+++ MVC_Three/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="MVC_Three.MvcApplication" Language="C#" %>
---
+++ MVC_Three/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(MVC_Three.Startup))]
+namespace MVC_Three
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
