bd270b06ef90028aaac14eaa1795af19040f3712
Farid <farid.skyman@gmail.com>
2018-04-18 22:57:31 +0800

    finished Exercise 2

---
+++ MVC_Three/Controllers/CustomersController.cs
@@ -0,0 +1,40 @@
+﻿using MVC_Three.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVC_Three.Controllers
+{
+    public class CustomersController : Controller
+    {
+        // GET: Customers
+        public ActionResult Index()
+        {
+          
+
+            return View(GetCustomers());
+        }
+
+        public ActionResult Details(int Id)
+        {
+            var customer = GetCustomers().SingleOrDefault(s => s.Id == Id);
+
+            if (customer == null)
+                return HttpNotFound();
+            else
+                return View(customer);
+        }
+
+
+        private IEnumerable<Customer> GetCustomers()
+        {
+            return new List<Customer>
+            {
+                new Customer{ Id=1, Name = "John Smith" },
+                new Customer{ Id=2, Name = "Mary Williams" }
+            };
+        }
+    }
+}
