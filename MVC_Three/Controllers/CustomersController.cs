1a4090d2cc4344dba0f57913d9cbc021c73d0215
Farid <farid.skyman@gmail.com>
2018-04-21 10:58:25 +0800

    End of Lecture 31 - Added data annotation - updated fields in customer details and index - fix a strange bug from customer index to details, where we need to specify /customer/details - update layout text - added EAGER LOADING for MEMBERSHIPTYPE when loading CUSTOMERS

--- MVC_Three/Controllers/CustomersController.cs
+++ MVC_Three/Controllers/CustomersController.cs
@@ -4,11 +4,28 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using System.Data.Entity;
 
 namespace MVC_Three.Controllers
 {
     public class CustomersController : Controller
     {
+        //declare dbcontext to connect to database
+        private ApplicationDbContext _context;
+
+        //ctor (shortcut to gen constructor)
+        public CustomersController()
+        {
+            _context = new ApplicationDbContext();
+        }
+
+        //dispose appdbcontext when no longer in use 
+        //  need to override base class to do this.
+        protected override void Dispose(bool disposing)
+        {
+            _context.Dispose();
+        }
+
         // GET: Customers
         public ActionResult Index()
         {
@@ -30,11 +47,15 @@ namespace MVC_Three.Controllers
 
         private IEnumerable<Customer> GetCustomers()
         {
-            return new List<Customer>
-            {
-                new Customer{ Id=1, Name = "John Smith" },
-                new Customer{ Id=2, Name = "Mary Williams" }
-            };
+            //  1. .ToList()
+            //      when call toList when the dbcall is made
+            //      else db call is defered till in view
+            //  2. Eager Loading
+            //      This is to load refence table along with customer object
+            //      added the .Include() to EAGER LOAD MembershipType, 
+            //      For it to work, we need to add reference to 'using System.Data.Entity;'
+            var customers = _context.Customers.Include(c => c.MembershipType).ToList(); 
+            return customers;
         }
     }
 }
--- MVC_Three/Models/Customer.cs
+++ MVC_Three/Models/Customer.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
@@ -8,11 +9,16 @@ namespace MVC_Three.Models
     public class Customer
     {
         public int Id { get; set; }
+
+        [Required] /*make column name non nullable*/
+        [StringLength(255)] /*limit string length*/
         public string Name { get; set; }
+
         public bool IsSubscribedToNewsletter { get; set; }
 
         /*navi prop, to load an obj and its related obj from db*/
         public MembershipType MembershipType { get; set; }
+        
         //foreign key to MembershipType table.
         public byte MembershipTypeId { get; set; }
     }
